0d3a14e4e8003fa6f1e416416fad7bc9ecc46318
sga <sorento_alba@list.ru>
2026-03-21 22:05:57 +0800

    feature/init-kernal-service

---
+++ backend/kernal-service/src/main/java/com/mis/kernal/KernalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mis.kernal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernalServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(KernalServiceApplication.class, args);
+	}
+
+}
